b6401f30ac29b3e2d6ae5256a79da9dcfc60b0e8
Ty. Trương Văn <truongvanty1608@gmail.com>
2025-12-31 17:35:23 +0700

    Update example

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 #!/usr/bin/env bun
 
-import type { IAsset, IAssetConfig, IOuputAsset } from "./types";
+import type { IAsset, IAssetConfig, IAssetItem, IOuputAsset } from "./types";
+export * from "./types";
+
 import { readdir } from "node:fs/promises";
 
 const configFileName = "asset.config.json";
-const REGEX_EXTENSION_ASSET = /\.(svg|png|apng|jpg|jpeg|webp|json|riv|ttf)$/gm;
+
+const FILE_SUPPORORTED = [
+  "svg",
+  "png",
+  "apng",
+  "jpg",
+  "jpeg",
+  "webp",
+  "json",
+  "riv",
+] as const;
+
+type ExtType = (typeof FILE_SUPPORORTED)[number];
+
+const REGEX_EXTENSION_ASSET = RegExp(
+  `\.(${FILE_SUPPORORTED.join("|")})$`,
+  "gm",
+);
 
 enum FileType {
   SVG = "svg",
@@ -14,68 +33,6 @@ enum FileType {
   WEBP = "webp",
   JSON = "json",
   RIVE = "riv",
-}
-
-function capitalize(text: string) {
-  const arraySplitString = text.trim().split(/[-_\s]/);
-
-  return arraySplitString.reduce((pre, next) => {
-    return pre + next.slice(0, 1).toUpperCase() + next.slice(1, next.length);
-  }, "");
-}
-
-async function processFolderAsset(asset: IAsset): Promise<IOuputAsset> {
-  const pathDir = asset.pathDir;
-
-  const fullPath = process.cwd() + pathDir;
-
-  let fileDataContent = "";
-
-  const filesName = await readdir(fullPath);
-
-  for (const index in filesName) {
-    const file = filesName[index];
-
-    if (!file) {
-      continue;
-    }
-
-    /// ignore scale up files @2x, @3x
-    if (/@[\d,.]+x/gm.test(file)) {
-      continue;
-    }
-
-    /// validate các extension file support
-    if (!REGEX_EXTENSION_ASSET.test(file)) {
-      continue;
-    }
-
-    const newData = {
-      path: file,
-      name: capitalize(file.replace(REGEX_EXTENSION_ASSET, "")),
-    };
-
-    /// set record to export
-    fileDataContent += `
-  /** How it display
-    *
-    * ![${newData.name}](${fullPath + "/" + newData.path})
-    * */
-  ${
-    newData.name[0]!.toLowerCase() + newData.name.slice(1, newData.name.length)
-  }: require("./${newData.path}"),`;
-  }
-
-  fileDataContent = `export const ${asset.assetName} = {${fileDataContent}\n};
-
-export type ${capitalize(
-    [asset.assetName, "type"].join(" "),
-  )} = keyof typeof ${asset.assetName};\n`;
-
-  return {
-    fileDataContent: fileDataContent,
-    genFilePath: fullPath + "/index.ts",
-  };
 }
 
 // main entry
@@ -100,3 +57,66 @@ export type ${capitalize(
     }),
   );
 })();
+
+async function processFolderAsset(asset: IAsset): Promise<IOuputAsset> {
+  const pathDir = asset.pathDir;
+
+  const fullPath = process.cwd() + pathDir;
+
+  let fileDataContent = "";
+
+  const filesName = await readdir(fullPath);
+
+  for (const index in filesName) {
+    const file = filesName[index];
+
+    if (!file) {
+      continue;
+    }
+
+    /// ignore scale up files @2x, @3x
+    if (/@[\d,.]+x/gm.test(file)) {
+      continue;
+    }
+
+    /// validate file's extension supported
+    if (!REGEX_EXTENSION_ASSET.test(file)) {
+      continue;
+    }
+
+    const newData: IAssetItem<ExtType> = {
+      path: file,
+      name: Utils.capitalize(file.replace(REGEX_EXTENSION_ASSET, "")),
+    };
+
+    /// set record to export
+    fileDataContent += `
+  /** How it display
+    *
+    * ![${newData.name}](${fullPath + "/" + newData.path})
+    * */
+  ${
+    newData.name[0]!.toLowerCase() + newData.name.slice(1, newData.name.length)
+  }: require("./${newData.path}"),`;
+  }
+
+  fileDataContent = `export const ${asset.assetName} = {${fileDataContent}\n};
+
+export type ${Utils.capitalize(
+    [asset.assetName, "type"].join(" "),
+  )} = keyof typeof ${asset.assetName};\n`;
+
+  return {
+    fileDataContent: fileDataContent,
+    genFilePath: fullPath + "/index.ts",
+  };
+}
+
+const Utils = {
+  capitalize(text: string) {
+    const arraySplitString = text.trim().split(/[-_\s]/);
+    return arraySplitString.reduce((pre, next) => {
+      return pre + next.slice(0, 1).toUpperCase() + next.slice(1, next.length);
+    }, "");
+  },
+};
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,14 @@
 export interface IAsset {
   pathDir: string;
   assetName: string;
+
+  /**
+   * Optional support for web application with TS/JS ecosystem
+   *
+   * @value `true` will not apply `require` command for code gen
+   * @default false
+   */
+  webSupport?: boolean;
 }
 
 export interface IAssetConfig {
@@ -10,4 +18,11 @@ export interface IAssetConfig {
 export interface IOuputAsset {
   genFilePath: string;
   fileDataContent: string;
+}
+
+export interface IAssetItem<Ext> {
+  fileRelativePath: string;
+  filePath: string;
+  assetName: string;
+  fileExt: Ext;
 }
